ab7b04c5e0a48933b22268d2ee1001a561223a03
Jonathan Breton <jbreton@kronostechnologies.com>
2016-05-13 11:33:43 -0400

    Adds Writer\Console to factory

--- Kronos/Log/WriterFactory.php
+++ Kronos/Log/WriterFactory.php
@@ -2,6 +2,8 @@
 
 namespace Kronos\Log;
 
+use Kronos\Log\Adaptor\FileFactory;
+
 class WriterFactory {
 
 	/**
@@ -9,8 +11,13 @@ class WriterFactory {
 	 */
 	private $context_stringifier = null;
 
+
+	/**
+	 * @param $filename
+	 * @return Writer\File
+	 */
 	public function createFileWriter($filename) {
-		$writer = new Writer\File($filename, new Adaptor\File());
+		$writer = new Writer\File($filename, new FileFactory());
 		$writer->prependDatetime();
 		$writer->prependLogLevel();
 		$writer->setContextStringifier($this->getContextStringifier());
@@ -18,10 +25,24 @@ class WriterFactory {
 		return $writer;
 	}
 
+	/**
+	 * @param $application
+	 * @return Writer\Syslog
+	 */
 	public function createSyslogWriter($application) {
 		return new Writer\Syslog(new Adaptor\Syslog(), $application);
 	}
 
+	/**
+	 * @return Writer\Console
+	 */
+	public function createConsoleWriter() {
+		return new Writer\Console(new FileFactory());
+	}
+
+	/**
+	 * @return ContextStringifier
+	 */
 	public function getContextStringifier() {
 		if(!$this->context_stringifier) {
 			$this->context_stringifier = new ContextStringifier();
