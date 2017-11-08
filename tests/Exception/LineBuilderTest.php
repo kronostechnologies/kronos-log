f71bb8f97ea3e21c511cd6abc991919ce486cebd
jbreton <jbreton@kronostechnologies.com>
2017-11-08 11:42:34 -0500

    Tests clean up

--- tests/Exception/LineBuilderTest.php
+++ tests/Exception/LineBuilderTest.php
@@ -2,19 +2,22 @@
 
 namespace Kronos\Tests\Log;
 
+use \Kronos\Log\Exception\LineBuilder;
+
 class LineBuilderTest extends \PHPUnit_Framework_TestCase {
 
 	const A_LINE_NB = 0;
-	const A_FILE_PATH = '/path/to/file/Testerino.php';
+	const A_FILE_PATH = '/path/to/file/TestClass.php';
 	const A_LINE = 20;
-	const A_CLASS = 'Testerino';
+	const A_CLASS = 'TestClass';
 	const A_TYPE = '->';
-	const A_FUNCTION = 'a';
+	const A_FUNCTION = 'testFunction';
 	const SOME_ARGS = [1,2,['test']];
 
-	public function test_givenACompleteSetOfExceptionTraceElements_buildExceptionString_shouldReturnAFormattedLineWithAllElementsAndArguments(){
-		$line_builder = new \Kronos\Log\Exception\LineBuilder();
+	const EMPTY_LINE = "";
 
+	public function test_givenACompleteSetOfExceptionTraceElements_buildExceptionString_shouldReturnAFormattedLineWithAllElementsAndArguments(){
+		$line_builder = new LineBuilder();
 		$line_builder->setLineNb(self::A_LINE_NB);
 		$line_builder->setFile(self::A_FILE_PATH);
 		$line_builder->setLine(self::A_LINE);
@@ -25,12 +28,11 @@ class LineBuilderTest extends \PHPUnit_Framework_TestCase {
 
 		$line = $line_builder->buildExceptionString();
 
-		$this->assertEquals($this->thenAFormattedLineWithAllElementsAndArguments(), $line);
+		$this->assertEquals('#'.self::A_LINE_NB.' '.self::A_FILE_PATH.'('.self::A_LINE.'): '.self::A_CLASS.self::A_TYPE.self::A_FUNCTION.'(1,2,Array)', $line);
 	}
 
 	public function test_givenASetOfExceptionTraceElementWithoutArgs_buildExceptionString_shouldReturnAFormattedLineWithAllElementsAndNoArguments(){
-		$line_builder = new \Kronos\Log\Exception\LineBuilder();
-
+		$line_builder = new LineBuilder();
 		$line_builder->setLineNb(self::A_LINE_NB);
 		$line_builder->setFile(self::A_FILE_PATH);
 		$line_builder->setLine(self::A_LINE);
@@ -40,12 +42,11 @@ class LineBuilderTest extends \PHPUnit_Framework_TestCase {
 
 		$line = $line_builder->buildExceptionString();
 
-		$this->assertEquals($this->thenAFormattedLineWithAllElementsAndNoArguments(), $line);
+		$this->assertEquals('#'.self::A_LINE_NB.' '.self::A_FILE_PATH.'('.self::A_LINE.'): '.self::A_CLASS.self::A_TYPE.self::A_FUNCTION.'()', $line);
 	}
 
 	public function test_givenASetOfExceptionTraceElementWithoutArgsAndFunction_buildExceptionString_shouldReturnAFormattedLineWithAllElementsAndNoFunctionOrType(){
-		$line_builder = new \Kronos\Log\Exception\LineBuilder();
-
+		$line_builder = new LineBuilder();
 		$line_builder->setLineNb(self::A_LINE_NB);
 		$line_builder->setFile(self::A_FILE_PATH);
 		$line_builder->setLine(self::A_LINE);
@@ -53,53 +54,47 @@ class LineBuilderTest extends \PHPUnit_Framework_TestCase {
 
 		$line = $line_builder->buildExceptionString();
 
-		$this->assertEquals($this->thenAFormattedLineWithAllElementsAndNoFunctionOrType(), $line);
+		$this->assertEquals('#'.self::A_LINE_NB.' '.self::A_FILE_PATH.'('.self::A_LINE.'): '.self::A_CLASS, $line);
 	}
 
 	public function test_givenASetOfExceptionTraceElementWithLineNbAndFileAndLine_buildExceptionString_shouldReturnAFormattedLineWithAllElementsAndNoClass(){
-		$line_builder = new \Kronos\Log\Exception\LineBuilder();
-
+		$line_builder = new LineBuilder();
 		$line_builder->setLineNb(self::A_LINE_NB);
 		$line_builder->setFile(self::A_FILE_PATH);
 		$line_builder->setLine(self::A_LINE);
-
 		$line = $line_builder->buildExceptionString();
 
-		$this->assertEquals($this->thenAFormattedLineWithAllElementsAndNoClass(), $line);
+		$this->assertEquals('#'.self::A_LINE_NB.' '.self::A_FILE_PATH.'('.self::A_LINE.'): ', $line);
 	}
 
 	public function test_givenASetOfExceptionTraceElementWithLineNbAndFile_buildExceptionString_shouldReturnAFormattedLineWithAllElementsAndNoLineNb(){
-		$line_builder = new \Kronos\Log\Exception\LineBuilder();
-
+		$line_builder = new LineBuilder();
 		$line_builder->setLineNb(self::A_LINE_NB);
 		$line_builder->setFile(self::A_FILE_PATH);
 
 		$line = $line_builder->buildExceptionString();
 
-		$this->assertEquals($this->thenAFormattedLineWithAllElementsAndNoLineNb(), $line);
+		$this->assertEquals('#'.self::A_LINE_NB.' '.self::A_FILE_PATH, $line);
 	}
 
 	public function test_givenASetOfExceptionTraceElementWithOnlyLineNb_buildExceptionString_shouldReturnAFormattedLineWithAllElementsAndNoFile(){
-		$line_builder = new \Kronos\Log\Exception\LineBuilder();
-
+		$line_builder = new LineBuilder();
 		$line_builder->setLineNb(self::A_LINE_NB);
 
 		$line = $line_builder->buildExceptionString();
 
-		$this->assertEquals($this->thenAFormattedLineWithAllElementsAndNoFile(), $line);
+		$this->assertEquals('#'.self::A_LINE_NB.' ', $line);
 	}
 
 	public function test_givenNoExceptionTraceElement_buildExceptionString_shouldReturnAnEmptyLine(){
-		$line_builder = new \Kronos\Log\Exception\LineBuilder();
-
+		$line_builder = new LineBuilder();
 		$line = $line_builder->buildExceptionString();
 
-		$this->assertEquals($this->thenAnEmptyLine(), $line);
+		$this->assertEquals(self::EMPTY_LINE, $line);
 	}
 
-	public function test_a_clearLine_shouldReturnAnEmptyLine2(){
-		$line_builder = new \Kronos\Log\Exception\LineBuilder();
-
+	public function test_clearLine_buildExceptionString_shouldReturnAnEmptyLine(){
+		$line_builder = new LineBuilder();
 		$line_builder->setLineNb(self::A_LINE_NB);
 		$line_builder->setFile(self::A_FILE_PATH);
 		$line_builder->setLine(self::A_LINE);
@@ -107,42 +102,12 @@ class LineBuilderTest extends \PHPUnit_Framework_TestCase {
 		$line_builder->setType(self::A_TYPE);
 		$line_builder->setFunction(self::A_FUNCTION);
 		$line_builder->setArgs(self::SOME_ARGS);
-
-		$line = $line_builder->buildExceptionString();
-
+		$line_builder->buildExceptionString();
 		$line_builder->clearLine();
 
 		$line = $line_builder->buildExceptionString();
 
-		$this->assertEquals($this->thenAnEmptyLine(), $line);
-	}
-
-	public function thenAFormattedLineWithAllElementsAndArguments(){
-		return '#0 /path/to/file/Testerino.php(20): Testerino->a(1,2,Array)';
-	}
-
-	public function thenAFormattedLineWithAllElementsAndNoArguments(){
-		return '#0 /path/to/file/Testerino.php(20): Testerino->a()';
-	}
-
-	public function thenAFormattedLineWithAllElementsAndNoFunctionOrType(){
-		return '#0 /path/to/file/Testerino.php(20): Testerino';
-	}
-
-	public function thenAFormattedLineWithAllElementsAndNoClass(){
-		return '#0 /path/to/file/Testerino.php(20): ';
-	}
-
-	public function thenAFormattedLineWithAllElementsAndNoLineNb(){
-		return '#0 /path/to/file/Testerino.php';
-	}
-
-	public function thenAFormattedLineWithAllElementsAndNoFile(){
-		return '#0 ';
-	}
-
-	public function thenAnEmptyLine(){
-		return "";
+		$this->assertEquals(self::EMPTY_LINE, $line);
 	}
 
 }
