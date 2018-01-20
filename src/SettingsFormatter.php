9cadfe472906cef00464262f22631f68db4d8e78
ogermain <=>
2018-01-20 17:15:42 -0500

    commence a gérer des flags pour permettre ou non a un writer de logger dans un tool dependemment des flags verbose/debug/dry-run

--- src/SettingsFormatter.php
+++ src/SettingsFormatter.php
@@ -18,6 +18,11 @@ class SettingsFormatter {
 	 */
 	private $changes = [];
 
+	/**
+	 * @var null|\Tool
+	 */
+	private $tool = null;
+
 	const WRITER_TYPE = 'type';
 
 	const WRITER_SETTINGS = 'settings';
@@ -28,9 +33,84 @@ class SettingsFormatter {
 	 * @param array $settings
 	 * @param array $changes
 	 */
-	public function __construct(array $settings = [], array $changes = []) {
+	public function __construct(array $settings = [], array $changes = [], \Tool $tool = null) {
 		$this->settings = $settings;
 		$this->changes = $changes;
+		$this->tool = $tool;
+	}
+
+	/**
+	 * @return array
+	 */
+	private function getToolCurrentLogModesTuple(){
+		$active_modes = [];
+		$inactive_modes = [];
+
+		$current_log_modes = [
+			'verbose' => $this->tool->isVerbose(),
+			'dry-run' => $this->tool->isDryRun(),
+			'debug' => $this->tool->isDebug()
+		];
+
+		foreach($current_log_modes as $log_mode => $is_activated){
+			if ($is_activated){
+				$active_modes[] = $log_mode;
+			}
+			else if (!$is_activated){
+				$inactive_modes[] = $log_mode;
+			}
+		}
+
+		return [
+			'active_modes' => $active_modes,
+			'inactive_modes' => $inactive_modes
+		];
+	}
+
+	/**
+	 * @param $writers
+	 * @param $writer
+	 * @param $key
+	 */
+	private function markUnallowedWritersToDelete(&$writers, &$writer, $key){
+		$writers[$key]['to_delete'] = false;
+
+		$tool_log_modes = $this->getToolCurrentLogModesTuple();
+
+		$activate_array = $writer[self::WRITER_SETTINGS]['activateOnlyWithFlag'] ?: [];
+		$deactivate_array = $writer[self::WRITER_SETTINGS]['deactivateOnlyWithFlag'] ?: [];
+
+		foreach($tool_log_modes as $tool_log_mode_name => $tool_log_mode){
+			$this->markToDelete($writers, $key, $tool_log_mode, ($tool_log_mode_name == 'active_modes') ? $activate_array : $deactivate_array);
+		}
+	}
+
+	/**
+	 * @param $writers
+	 * @param $key
+	 * @param $tool_log_modes
+	 * @param $config_log_modes
+	 */
+	private function markToDelete(&$writers, $key, $tool_log_modes, $config_log_modes){
+		if (!empty($tool_log_modes)){
+			foreach ($tool_log_modes as $tool_log_mode){
+				if (in_array($tool_log_mode, $config_log_modes)){
+					$writers[$key]['to_delete'] = true;
+				}
+			}
+		}
+	}
+
+	/**
+	 * @param $writers
+	 * @param $key
+	 */
+	private function deleteMarkedWritersToDelete(&$writers, $key){
+		foreach ($writers as $writer){
+			if ($writer['to_delete']){
+				unset($writers[$key]);
+			}
+		}
 	}
 
 	/**
@@ -44,7 +124,13 @@ class SettingsFormatter {
 		if (!empty($this->changes)){
 			foreach ($this->changes as $writer_type => $writer_settings){
 				if (!empty($this->settings)){
-					foreach($writers as &$writer) {
+					foreach($writers as $key => &$writer) {
+
+						if (!is_null($this->tool)) {
+							$this->markUnallowedWritersToDelete($writers, $writer, $key);
+							$this->deleteMarkedWritersToDelete($writers, $key);
+						}
+
 						if ($writer[self::WRITER_TYPE] == $writer_type){
 							foreach ($writer_settings as $setting_name => $setting_value){
 								$writer[self::WRITER_SETTINGS][$setting_name] = $setting_value;
@@ -54,9 +140,9 @@ class SettingsFormatter {
 				}
 			}
 		}
-
 		return $writers;
 	}
+
 
 	/**
 	 * Gets the writers options of the 'log' config settings
