2e71376bd90ad96e149b2b9f4ad3e1ed9f10e8ea
Eric Durand-Tremblay <etremblay@kronostechnologies.com>
2018-12-05 11:08:44 -0500

    Support for exception metadata in fluentd logger

--- src/Writer/Fluentd.php
+++ src/Writer/Fluentd.php
@@ -6,9 +6,14 @@ namespace Kronos\Log\Writer;
 
 use Fluent\Logger\FluentLogger;
 use Kronos\Log\AbstractWriter;
+use Kronos\Log\Formatter\ContextStringifier;
+use Kronos\Log\Formatter\Exception\TraceBuilder;
+use Kronos\Log\Traits\ExceptionTraceBuilderAwareTrait;
 
 class Fluentd extends AbstractWriter
 {
+    use ExceptionTraceBuilderAwareTrait;
+
     /**
      * @var string
      */
@@ -45,33 +50,59 @@ class Fluentd extends AbstractWriter
     protected $wrapContextInMeta;
 
     /**
+     * @var TraceBuilder
+     */
+    private $exceptionTraceBuilder;
+
+    /**
+     * @var TraceBuilder
+     */
+    private $previousExceptionTraceBuilder;
+
+    /**
+     * @var ContextStringifier
+     */
+    private $contextStringifier;
+
+    /**
      * @param string $hostname
      * @param int $port
      * @param $tag
      * @param null|string $application
      * @param bool $wrapContextInMeta
      * @param \Kronos\Log\Factory\Fluentd|null $factory
+     * @param ContextStringifier|null $contextStringifier
      */
-    public function __construct($hostname, $port, $tag, $application, $wrapContextInMeta, \Kronos\Log\Factory\Fluentd $factory = null)
-    {
+    public function __construct(
+        $hostname,
+        $port,
+        $tag,
+        $application,
+        $wrapContextInMeta,
+        \Kronos\Log\Factory\Fluentd $factory = null,
+        ContextStringifier $contextStringifier = null
+    ) {
         $this->hostname = $hostname;
         $this->port = $port;
         $this->tag = $tag;
         $this->application = $application;
         $this->wrapContextInMeta = $wrapContextInMeta;
         $this->factory = $factory ?: new \Kronos\Log\Factory\Fluentd();
+        $this->contextStringifier = $contextStringifier ?: new ContextStringifier();
     }
 
     public function log($level, $message, array $context = [])
     {
         try {
-             $logger = $this->initializeLogger();
+            $logger = $this->initializeLogger();
+
+            $context = $this->processContext($context);
 
             $context['level'] = $level;
             if ($this->application !== null) {
                 $context['_app'] = $this->application;
             }
-            $context['message'] = $message;
+            $context['message'] = $this->interpolate($message, $context);
 
             if ($this->wrapContextInMeta) {
                 $context['meta'] = $context;
@@ -84,9 +115,15 @@ class Fluentd extends AbstractWriter
 
             return true;
         } catch (\Exception $ex) {
-            trigger_error('An error occurred while writing with the Fluentd writer: ' . $ex->getMessage(), E_USER_WARNING);
+            trigger_error('An error occurred while writing with the Fluentd writer: ' . $ex->getMessage(),
+                E_USER_WARNING);
             return false;
         }
+    }
+
+    private function processContext(array $context){
+        $context = $this->replaceException($context);
+        return $this->contextStringifier->stringifyArray($context);
     }
 
     /**
@@ -140,4 +177,53 @@ class Fluentd extends AbstractWriter
     {
         return $this->wrapContextInMeta;
     }
+
+    /**
+     * @return ContextStringifier
+     */
+    public function getContextStringifier()
+    {
+        return $this->contextStringifier;
+    }
+
+    /**
+     * @param ContextStringifier $contextStringifier
+     */
+    public function setContextStringifier(ContextStringifier $contextStringifier)
+    {
+        $this->contextStringifier = $contextStringifier;
+    }
+
+    /**
+     * @return TraceBuilder
+     */
+    public function getExceptionTraceBuilder()
+    {
+        return $this->exceptionTraceBuilder;
+    }
+
+    /**
+     * @param TraceBuilder $exceptionTraceBuilder
+     */
+    public function setExceptionTraceBuilder($exceptionTraceBuilder)
+    {
+        $this->exceptionTraceBuilder = $exceptionTraceBuilder;
+    }
+
+    /**
+     * @return TraceBuilder
+     */
+    public function getPreviousExceptionTraceBuilder()
+    {
+        return $this->previousExceptionTraceBuilder;
+    }
+
+    /**
+     * @param TraceBuilder $previousExceptionTraceBuilder
+     */
+    public function setPreviousExceptionTraceBuilder($previousExceptionTraceBuilder)
+    {
+        $this->previousExceptionTraceBuilder = $previousExceptionTraceBuilder;
+    }
+
 }
