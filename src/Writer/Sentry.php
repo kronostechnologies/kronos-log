9df2823989dc2bf498ce543c38f95a833fbb6920
Antoine Giasson <antoine.giasson@equisoft.com>
2023-07-06 21:52:26 -0400

    2nd attempt at fixing sentry writer test without modifying writer

--- src/Writer/Sentry.php
+++ src/Writer/Sentry.php
@@ -9,16 +9,9 @@ use Psr\Log\LogLevel;
 use Sentry\ClientInterface;
 use Sentry\Severity;
 use Sentry\State\Scope;
-use Sentry\Tracing\PropagationContext;
-use Sentry\Tracing\SpanId;
-use Sentry\Tracing\TraceId;
 
 class Sentry extends AbstractWriter
 {
-
-    const SPAN_ID = "3d1bf6350d09fb80";
-    const TRACE_ID = "141bb800f59d073b7a075b1eed7d5372";
-
     /**
      * @var ClientInterface
      */
@@ -71,30 +64,16 @@ class Sentry extends AbstractWriter
         $this->sentryClient->captureException($exception, $sentryScope);
     }
 
-    private function getSentryScope($level, $context): Scope
+    protected function getSentryScope($level, $context): Scope
     {
         $scope = new Scope();
         $scope->setLevel($level);
-
-        if (isset($context['test'])) {
-            $this->setPropagationContext($scope);
-            unset($context['test']);
-        }
 
         if (count($context)) {
             $scope->setExtras($context);
         }
 
         return $scope;
-    }
-
-    private function setPropagationContext(Scope $scope): void
-    {
-        $propagationContext = PropagationContext::fromDefaults();
-        $propagationContext->setSpanId(new SpanId(self::SPAN_ID));
-        $propagationContext->setTraceId(new TraceId(self::TRACE_ID));
-
-        $scope->setPropagationContext($propagationContext);
     }
 
     /**
